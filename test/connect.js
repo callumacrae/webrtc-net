d4a834f63ecd1ee52abde6ef622a071db2202978
Callum Macrae <callum@samknows.com>
2016-11-13 00:07:30 +0000

    Added test with multiple peers

--- test/connect.js
+++ test/connect.js
@@ -15,4 +15,39 @@ describe('Peernet simple connection', () => {
 			done();
 		});
 	});
+
+	it('should connect twice!', (done) => {
+		const peernet1 = new PeerNet();
+		const peernet2 = new PeerNet();
+		const peernet3 = new PeerNet();
+
+		peernet1.getToken()
+			.then((token) => peernet3.invite(token));
+
+		peernet2.getToken()
+			.then((token) => peernet3.invite(token));
+
+		let connected = 0;
+
+		peernet1.on('connect', () => {
+			connected++;
+			if (connected === 2) {
+				finishTest();
+			}
+		});
+
+		peernet2.on('connect', () => {
+			connected++;
+			if (connected === 2) {
+				finishTest();
+			}
+		});
+
+		function finishTest() {
+			peernet1.directPeers.length.should.equal(1);
+			peernet2.directPeers.length.should.equal(1);
+			peernet3.directPeers.length.should.equal(2);
+			done();
+		}
+	});
 });
